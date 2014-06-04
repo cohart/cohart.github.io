ffa9719037b7f3fe97d3de7c0a56ec9b6b5c72cf
cohart <me@colin-hart.com>
2014-06-04 19:27:58 -0400

    moves jquery into the right place

--- assets/scripts/nav_hover.js
+++ assets/scripts/nav_hover.js
@@ -1,13 +1,14 @@
- <script>
-        $("#about").hover(function() {
-            $( this ).append( $( "<span> ***</span>" ) );
-          }, function() {
-            $( this ).find( "span:last" ).remove();
-          }
-        );
+$().ready(function() {
+  $("#about").mouseenter(function() {
+      $( "#content" ).empty();
+      $("#content").append( $( "<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, quibusdam, atque cum consequuntur suscipit ea facilis hic deserunt voluptatibus praesentium aperiam cupiditate tempora neque voluptatem perferendis iusto aliquam. Iste, magnam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, earum, facere expedita itaque eius commodi amet incidunt minima aliquid cupiditate quod reprehenderit voluptate iusto aut aliquam inventore sapiente voluptates quia. </span>" ).fadeIn("slow") );
+    }
+  );
 
-        $( "li.fade" ).hover(function() {
-          $( this ).fadeOut( 100 );
-          $( this ).fadeIn( 500 );
-        });
-  </script>
+  $("#resume").mouseenter(function() {
+      $( "#content" ).empty();
+      $("#content").append( $( "<a href='assets/web/viewer.html?file=web-resume.pdf'><img src='assets/images/resume-clip.png' id='resume-clip' alt=''></a>" ).fadeIn("slow") );
+      $("#content").append( $( "<div id='link-to-resume'> <a href='assets/web/viewer.html?file=web-resume.pdf'>click for full resume</a></div>" ).fadeIn("slow") );
+    }
+  );
+});
