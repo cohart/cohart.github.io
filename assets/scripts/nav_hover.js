90ef91dd061b64cd0360a5e3ae78b64cdfc0dd72
cohart <me@colin-hart.com>
2014-06-06 16:50:30 -0400

    final changes minus tweetsmithy

--- assets/scripts/nav_hover.js
+++ assets/scripts/nav_hover.js
@@ -8,14 +8,15 @@ $().ready(function() {
   $("#resume").mouseenter(function() {
       $( "#content" ).empty();
       $("#content").append( $( "<a href='assets/web/viewer.html?file=web-resume.pdf'><img src='assets/images/resume-clip.png' id='resume-clip' alt=''></a>" ).fadeIn("slow") );
-      $("#content").append( $( "<div id='link-to-resume'> <a href='assets/web/viewer.html?file=web-resume.pdf'>click for full resume</a></div>" ).fadeIn("slow") );
+      $("#content").append( $( "<div id='link-to-resume'> <a href='assets/web/viewer.html?file=web-resume.pdf'>Full Resume</a></div>" ).fadeIn("slow") );
     }
   );
 
   $("#seedly").mouseenter(function() {
       $( "#content" ).empty();
-      $("#content").append( $( "<div><a href=http://seed-ly.herokuapp.com/'><img src='assets/images/seedly-logo.png' id='seedly-logo' alt='seedly logo'></a></div>" ).fadeIn("slow") );
+      $("#content").append( $( "<div><img src='assets/images/seedly-logo1.png' id='seedly-logo' alt='seedly logo'></div>" ).fadeIn("slow") );
       $("#content").append( $( "<div id='seedly-content'>Seedly is an app for gardeners that lets the user easily find their local frost date, calculate when individual seeds should be planted.</div>" ).fadeIn("slow") );
+      $("#content").append( $( "<div id='seedly-links'><i class='fa fa-github-alt'></i><a href='https://github.com/cohart/seedly'>Source</a> | <a href='http://seed-ly.herokuapp.com/'>Seedly</a></div>" ).fadeIn("slow") );
     }
   );
 
@@ -25,11 +26,10 @@ $().ready(function() {
     }
   );
 
-
   $("#smooth-streets").mouseenter(function() {
     $( "#content" ).empty();
-    $(" #content ").append( $( "<a href='http://smoothstreets.herokuapp.com'><img src='assets/images/smooth-streets.png' id='smooth-streets-pic' alt='smooth streets'></a>" ).fadeIn("slow") );
-    $("#content").append( $( "<div id='link-to-challenge-post'> <a href='http://challengepost.com/software/smooth-streets'>Challenge Post</a></div>" ).fadeIn("slow") );
-  }
+    $(" #content ").append( $( "<img src='assets/images/smooth-streets.png' id='smooth-streets-pic' alt='smooth streets'>" ).fadeIn("slow") );
+    $("#content").append( $( "<div id='smooth-streets-links'><i class='fa fa-github-alt'></i><a href='https://github.com/trivett/smooth_streets'>Source</a> | <a href='http://smoothstreets.herokuapp.com/'>SmoothStreets</a> | <a href='http://challengepost.com/software/smooth-streets '>Challenge Post</a></div>" ).fadeIn("slow") );
+    }
   );
 });
