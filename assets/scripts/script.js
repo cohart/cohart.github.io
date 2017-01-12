ea1033b90101ddac2329f79fa448ba6f0ff45b49
Colin Hart <me@colin-hart.com>
2017-01-12 16:08:22 -0500

    first round

---
+++ assets/scripts/script.js
@@ -0,0 +1,51 @@
+$(function() {
+  const $nav = $('nav');
+
+  $nav.click(function(event) {
+    switch (event.target.className) {
+      case "about":
+        uiObject.loadAbout();
+        break;
+      case "portfolio":
+        uiObject.loadPortfolio();
+        break;
+      case "contact":
+        uiObject.loadContact();
+        break;
+      case "logo":
+        break;
+      default:
+        console.log('default')
+    }
+  });
+})
+
+
+const uiObject = {
+  toggle() {
+    $(event.target).parent().children('.active').removeClass('active');
+
+    $('section:visible').toggle();
+  },
+
+  loadAbout() {
+    this.toggle()
+
+    $(event.target).addClass('active');
+    $('.about-content').toggle();
+  },
+
+  loadPortfolio() {
+    this.toggle()
+
+    $(event.target).addClass('active');
+    // $('.about-content').toggle();
+  },
+
+  loadContact() {
+    this.toggle()
+
+    $(event.target).addClass('active');
+    console.log("contact")
+  }
+}
