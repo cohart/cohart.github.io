5513827ead82327c153c6a42c2533b6ea7e58279
Colin Hart <me@colin-hart.com>
2017-01-12 22:03:42 -0500

    resolve toggle promise issue

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -22,49 +22,42 @@ $(function() {
 
 
 const uiObject = {
-  toggle(fader, resolve) {
+  toggle(fader, target, resolve) {
     $(event.target).parent().children('.active').removeClass('active');
     // NEEDS A PROMISE
     if ($('section:visible').length) {
-      $('section:visible').fadeToggle('fast', (resolve) => {
-        $(fader).fadeToggle('fast', (resolve) => {
+      $('section:visible').fadeToggle('fast', () => {
+        $(target).addClass('active');
+
+        $(fader).fadeToggle('fast', () => {
           resolve()
         });
       });
     } else {
-      debugger
-      resolve()
+      $(target).addClass('active');
+      resolve($(fader).fadeToggle('fast'))
     }
   },
 
   loadAbout() {
+    var target = event.target;
     var faded = new Promise((resolve, reject) => {
-      this.toggle('.about-content', resolve)
+      this.toggle('.about-content', target, resolve)
     })
 
-    faded.then(() => {
-      $('.about-content').toggle();
-    })
-    .then(() => {
-      $(event.target).addClass('active');
-    })
-    .catch((err) => {
+    faded.catch((err) => {
       console.error(err)
     })
   },
 
   loadPortfolio() {
+    var target = event.target;
+
     var faded = new Promise((resolve, reject) => {
-      this.toggle('.portfolio-content', resolve)
+      this.toggle('.portfolio-content', target ,resolve)
     })
 
     faded.then(() => {
-      $('.portfolio-content').toggle();
-    })
-    .then(() => {
-      $(event.target).addClass('active');
-    })
-    .then(() => {
       var t = setInterval( function() {
         $("#carousel ul").animate({marginLeft: -480}, 2000 , function() {
           $(this).find("li:last").after($(this).find("li:first"));
@@ -75,13 +68,6 @@ const uiObject = {
     .catch((err) => {
       console.error(err)
     })
-
-    // this.toggle('.portfolio-content')
-    //
-    // $(event.target).addClass('active');
-    // $('.portfolio-content').toggle();
-
-    // Set the interval to be 2.5 seconds
   },
 
   loadContact() {
